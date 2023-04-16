9d7ca5c3c832a6aff39e0f99da6a61e63ea4d6fe
vanhieu154 <117420642+vanhieu154@users.noreply.github.com>
2023-04-16 21:43:24 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GAP 2002';
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Đẩy trang đến vị trí đầu trang
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
--- src/app/trangsp/trangsp.component.ts
+++ src/app/trangsp/trangsp.component.ts
@@ -1,23 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
 import { Router } from '@angular/router';
+import { IProduct } from '../product';
 
 @Component({
   selector: 'app-trangsp',
   templateUrl: './trangsp.component.html',
   styleUrls: ['./trangsp.component.css']
 })
-export class TrangspComponent {
+export class TrangspComponent implements OnInit{
   product=new Product();
-  products:any
+  products:IProduct[]=[];
   errMessage:string=''
   http: any;
+  perPage: number =12
+  page:number=1
+  numPage:number=1
+  showProducts:any
+  allPage:any=[];
+  tempProducts:IProduct[]=[];
+  brands:any=[];
+  selectedBrands: any = [];
+  selectedPrices: any = [];
+
   constructor(private _service: ProductService,private router:Router){
     this._service.getProducts().subscribe({
-      next:(data)=>{this.products=data},
+      next:(data:IProduct[])=>{
+        this.products=data;
+        this.page= Math.ceil(this.products.length/this.perPage);
+        this.showProducts=this.products.slice((this.numPage-1)*this.perPage,this.numPage*this.perPage)
+        for (let index = 0; index < this.page; index++) {
+          this.allPage.push(index+1);
+        }
+
+        this.tempProducts=this.products
+        for (let i = 0; i < this.products.length; i++) {
+          this.brands.push(this.products[i].Hang);
+        }
+        this.brands = [...new Set(this.products.map(item => item.Hang))];
+        console.log(this.brands);
+
+
+
+      },
       error:(err)=>{this.errMessage=err}
     })
   }
+  ngOnInit(): void {
+    // this.selectedBrands = {};
+  }
+
+
+  showAllProducts(){
+    this.tempProducts=this.products;
+    this.showProduct(1,this.products)
+  }
+
+  showTypeProducts(t:string){
+    this.tempProducts=this.tempProducts.filter(p => p.LoaiSP==t)
+    if(this.tempProducts.length==0){
+      this.tempProducts=this.products.filter(p => p.LoaiSP==t)
+    }
+    this.showProduct(1,this.tempProducts)
+  }
+
+
+  showProduct(n: number,a:any){
+    this.allPage=[];
+    this.numPage=n
+    window.scrollTo(0, 0);
+    this.showProducts=this.tempProducts.slice((n-1)*this.perPage,n*this.perPage)
+    this.page= Math.ceil(this.tempProducts.length/this.perPage);
+    for (let index = 0; index < this.page; index++) {
+      this.allPage.push(index+1);
+    }
+  }
+
+
+
+  productsFilter(){
+    this.selectedBrands = Object.keys(this.selectedBrands).filter(brand => this.selectedBrands[brand]);
+    console.log(this.selectedBrands); // mảng chứa các brand có giá trị true
+
+
+  }
+
+  nextPage(){
+    if(this.numPage<this.page){
+      this.numPage=this.numPage+1
+      window.scrollTo(0, 0);
+      this.showProducts=this.tempProducts.slice((this.numPage-1)*this.perPage,this.numPage*this.perPage)
+    }
+  }
+
+  prevPage(){
+    if(this.numPage>1){
+      this.numPage=this.numPage-1
+      window.scrollTo(0, 0);
+      this.showProducts=this.tempProducts.slice((this.numPage-1)*this.perPage,this.numPage*this.perPage)
+    }
+  }
+
+  Detail(p:any){
+    this.router.navigate(['chitietsp',p._id])
+  }
+
 
 }
