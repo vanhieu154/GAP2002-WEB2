909321178151c592616bd6fa44cebb00db729ace
vanhieu154 <117420642+vanhieu154@users.noreply.github.com>
2023-04-20 11:58:17 +0700

    update account page

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -6,6 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-page.component.css']
 })
 export class AccountPageComponent {
+  myProfile=false;
+  myAddress=false;
+  changePass=false;
+  myProfileP=true;
 
+  myOrder=true;
+  myOrderDetail=false;
 
+  waitConfirmOrder=true;
+  waitPickUp=false
+  inDelivering=false
+  delivered=false
+  cancelled=false
+
+  myNotification=false;
+
+  showmodal=false;
+  showMy(myProfile:boolean,myOrder:boolean,myNotification:boolean){
+    this.myProfile =myProfile;
+    this.myOrder=myOrder;
+    this.myNotification=myNotification;
+  }
+  showMyAccount(myProfileP:boolean,myAddress:boolean,changePass:boolean){
+    this.myAddress=myAddress;
+    this.changePass=changePass;
+    this.myProfileP=myProfileP;
+  }
+  showOrderStatus( waitConfirmOrder:boolean,waitPickUp:boolean,inDelivering:boolean,delivered:boolean,cancelled:boolean){
+    this.waitConfirmOrder=waitConfirmOrder;
+    this.waitPickUp=waitPickUp;
+    this.inDelivering=inDelivering;
+    this.delivered=delivered;
+    this.cancelled=cancelled;
+  }
+  showModal(){
+    this.showmodal=true
+  }
+  hideModal(){
+    this.showmodal=false
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,72 +2,46 @@ import { AfterViewInit, Component, OnDestroy, OnInit, Renderer2, ViewChild } fro
 import { Router } from '@angular/router';
 import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
 
-// @Directive({
-//   selector: '[clickOutside]'
-// })
-// export class ClickOutsideDirective {
-//   constructor(private _elementRef: ElementRef) { }
-
-//   @Output()
-//   public clickOutside = new EventEmitter();
-
-//   @HostListener('document:click', ['$event.target'])
-//   public onClick(targetElement: HTMLElement) {
-//     const clickedInside = this._elementRef.nativeElement.contains(targetElement);
-//     if (!clickedInside) {
-//       this.clickOutside.emit(null);
-//     }
-//   }
-// }
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
   @ViewChild('header') header!: ElementRef;
   isLoggedIn = false
   prevScrollpos = 0;
-
-  public showLoginBox = false;
-
-  public showLogin(): void {
-    this.showLoginBox = !this.showLoginBox;
-  }
-
-  public hideLogin(): void {
-    this.showLoginBox = false;
-    console.log(this.showLoginBox);
-
-  }
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
-
-  ngOnInit(): void {
-    document.addEventListener('click', (event) => {
-      const targetElement = event.target as HTMLElement;
-      if (targetElement && !this.elementRef.nativeElement.contains(targetElement)) {
-        this.showLoginBox = false;
-      }
-    });
+  showLoginBox = false;
+  showCartBox=false;
+  showSearchBox=false;
+  showBox(showCartBox: boolean, showLoginBox: boolean,showSearchBox:boolean): void {
+    if(this.showCartBox != showCartBox || this.showLoginBox != showLoginBox || this.showSearchBox!=showSearchBox){
+      this.showCartBox = showCartBox;
+      this.showLoginBox = showLoginBox;
+      this.showSearchBox= showSearchBox;
+    }
+    else{
+      this.hideBox()
+    }
   }
-  // ngOnDestroy(): void {
-  //   // document.removeEventListener('click', this.handleDocumentClick);
-  //   document.removeEventListener('click', this.onClickOutside);
-  // }
+  hideBox(): void {
+    this.showLoginBox = false;
+    this.showCartBox = false;
+    this.showSearchBox =false;
+  }
 
-  // onClickOutside = (event: Event) => {
-  //   // Kiểm tra xem phần tử được click có thuộc ô login hay không
-  //   if (!this.loginBox.nativeElement.contains(event.target)) {
-  //     // Ẩn ô login nếu click bên ngoài
-  //     this.showLoginBox = false;
-  //   }
-  // }
+  @HostListener('document:click', ['$event'])
+  onClick(event: any): void {
+    const targetElement = event.target as HTMLElement;
+    if (targetElement && !this.elementRef.nativeElement.contains(targetElement)) {
+      this.hideBox();
+    }
+  }
 
-  ngAfterViewInit() {
-    window.onscroll = () => {
-      const currentScrollPos = window.pageYOffset;
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const currentScrollPos = window.pageYOffset;
       if (this.prevScrollpos > currentScrollPos) {
         if (this.header) {
           this.renderer.setStyle(this.header.nativeElement, 'top', '0');
@@ -79,14 +53,28 @@ export class HeaderComponent implements OnInit {
         }
       }
       this.prevScrollpos = currentScrollPos;
-    };
   }
-  // showLogin(): void {
-  //   this.subnavActive = !this.subnavActive;
-  //   // this.subnavActive = true;
-  //   // Thêm event listener cho click của document
-  //   // document.addEventListener('click', this.onClickOutside);
+  // ngAfterViewInit() {
+  //   window.onscroll = () => {
+  //     const currentScrollPos = window.pageYOffset;
+  //     if (this.prevScrollpos > currentScrollPos) {
+  //       if (this.header) {
+  //         this.renderer.setStyle(this.header.nativeElement, 'top', '0');
+  //       }
+  //     } else {
+  //       if (this.header) {
+  //         this.renderer.setStyle(this.header.nativeElement, 'top', '-105px');
+  //         this.renderer.setStyle(this.header.nativeElement, 'marginTop', '0px');
+  //       }
+  //     }
+  //     this.prevScrollpos = currentScrollPos;
+  //   };
   // }
+
+
+
+
+
 
 
   showSubnav(): void {
