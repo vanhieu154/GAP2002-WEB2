c0b1b8df6fde01013e7d9c0ae725c10cb31e91ec
Vo-Nhung <nhungvt20406@st.uel.edu.vn>
2023-04-23 01:25:56 +0700

    apdate

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,17 +9,52 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   hide = true;
+  name = new FormControl('', [Validators.required]);
+  getErrorNameMessage() {
+    if (this.name.hasError('required')) {
+      return '*Vui lòng nhập họ và tên';
+    }
+
+    return this.name.hasError('name') ? 'Họ và tên không hợp lệ' : '';
+  }
+
+  acc = new FormControl('', [Validators.required]);
+  getErrorAccMessage() {
+    if (this.acc.hasError('required')) {
+      return '*Vui lòng nhập tên đăng nhập';
+    }
+
+    return this.acc.hasError('acc') ? 'Tên đăng nhập không hợp lệ' : '';
+  }
+
+  password = new FormControl('', [Validators.required]);
+  getErrorPWMessage() {
+    if (this.password.hasError('required')) {
+      return '*Vui lòng nhập mật khẩu';
+    }
+
+    return this.password.hasError('acc') ? 'Mật khẩu không hợp lệ' : '';
+  }
+
+  dob = new FormControl('', [Validators.required]);
+  getErrorDoBMessage() {
+    if (this.dob.hasError('required')) {
+      return '*Vui lòng nhập ngày sinh';
+    }
+
+    return this.dob.hasError('acc') ? 'Ngày sinh không hợp lệ' : '';
+  }
 
   phoneNumber = new FormControl('', [
     Validators.required,
-    Validators.pattern(/^(\d{10})$/),
+    Validators.pattern(/^0\d{9}$/),
   ]);
   getErrorPhoneMessage() {
     if (this.phoneNumber.hasError('required')) {
       return '*Vui lòng nhập số điện thoại';
     }
 
-    return this.phoneNumber.hasError('phone') ? 'Số điện thoại không hợp lệ' : '';
+    return this.phoneNumber.hasError('pattern') ? '*Số điện thoại không hợp lệ' : '';
   }
 
   email = new FormControl('', [Validators.required, Validators.email]);
