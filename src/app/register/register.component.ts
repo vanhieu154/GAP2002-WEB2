e3ee0466352039ddc7bab7d7848bb2e5cfceb977
Vo-Nhung <nhungvt20406@st.uel.edu.vn>
2023-04-23 00:12:35 +0700

    update dk

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +8,26 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent {
+  hide = true;
 
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(\d{10})$/),
+  ]);
+  getErrorPhoneMessage() {
+    if (this.phoneNumber.hasError('required')) {
+      return '*Vui lòng nhập số điện thoại';
+    }
+
+    return this.phoneNumber.hasError('phone') ? 'Số điện thoại không hợp lệ' : '';
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  getErrorEmailMessage() {
+    if (this.email.hasError('required')) {
+      return '*Vui lòng nhập email';
+    }
+
+    return this.email.hasError('email') ? '*Email không hợp lệ' : '';
+  }
 }
