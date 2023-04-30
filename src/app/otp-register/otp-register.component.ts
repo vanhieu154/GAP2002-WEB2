615f62fff524653b21bc2f4b5d52159ee2956166
thuynthuyn <118806972+thuynthuyn@users.noreply.github.com>
2023-04-30 19:03:04 +0700

    update

--- src/app/otp-register/otp-register.component.ts
+++ src/app/otp-register/otp-register.component.ts
@@ -37,33 +37,62 @@ export class OtpRegisterComponent {
   }
 
 }
+// function OTPInput() {
+//   const inputs = document.querySelectorAll<HTMLInputElement>('#otp > *[id]');
+
+//   for (let i = 0; i < inputs.length; i++) {
+//     inputs[i].addEventListener('keydown', function(event: KeyboardEvent) {
+//       if (event.key === "Backspace") {
+//         inputs[i].value = '';
+//         if (i !== 0)
+//           (inputs[i - 1] as HTMLInputElement).focus();
+//       } else {
+//         if (i === inputs.length - 1 && inputs[i].value !== '') {
+//           return true;
+//         } else if (event.keyCode > 47 && event.keyCode < 58) { //0-9 only
+//           inputs[i].value = event.key;
+//           if (i !== inputs.length - 1)
+//             (inputs[i + 1] as HTMLInputElement).focus();
+//           event.preventDefault();
+//         } else if (event.keyCode > 64 && event.keyCode < 91) {
+//           inputs[i].value = String.fromCharCode(event.keyCode);
+//           if (i !== inputs.length - 1)
+//             (inputs[i + 1] as HTMLInputElement).focus();
+//           event.preventDefault();
+//         }
+//       }
+//     });
+//   }
+//   return undefined;
+// }
+
+// OTPInput();
+
 function OTPInput() {
   const inputs = document.querySelectorAll<HTMLInputElement>('#otp > *[id]');
 
   for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keydown', function (event: KeyboardEvent) {
-        if (event.key === "Backspace") {
-          inputs[i].value = '';
-          if (i !== 0)
-            (inputs[i - 1] as HTMLInputElement).focus();
-        } else {
-          if (i === inputs.length - 1 && inputs[i].value !== '') {
-            return true;
-          } else if (event.keyCode > 47 && event.keyCode < 58) { //0-9 only
-            inputs[i].value = event.key;
-            if (i !== inputs.length - 1)
-              (inputs[i + 1] as HTMLInputElement).focus();
-            event.preventDefault();
-          } else if (event.keyCode > 64 && event.keyCode < 91) {
-            inputs[i].value = String.fromCharCode(event.keyCode);
-            if (i !== inputs.length - 1)
-              (inputs[i + 1] as HTMLInputElement).focus();
-            event.preventDefault();
-          }
+    inputs[i].addEventListener('keydown', function (event: KeyboardEvent): void {
+      if (event.key === "Backspace") {
+        inputs[i].value = '';
+        if (i !== 0)
+          (inputs[i - 1] as HTMLInputElement).focus();
+      } else {
+        if (i === inputs.length - 1 && inputs[i].value !== '') {
+          return ;
+        } else
+        if (event.keyCode > 47 && event.keyCode < 58) { //0-9 only
+          inputs[i].value = event.key;
+          if (i !== inputs.length - 1)
+            (inputs[i + 1] as HTMLInputElement).focus();
+          event.preventDefault();
+        } else if (event.keyCode > 64 && event.keyCode < 91) {
+          inputs[i].value = String.fromCharCode(event.keyCode);
+          if (i !== inputs.length - 1)
+            (inputs[i + 1] as HTMLInputElement).focus();
+          event.preventDefault();
         }
-      });
+      }
+    });
   }
-  return;
 }
-
-OTPInput();
