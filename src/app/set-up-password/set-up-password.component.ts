e00cc0131d8712b3d4d4c6a0809b1cb4ae279b68
thuynthuyn <118806972+thuynthuyn@users.noreply.github.com>
2023-04-30 17:21:33 +0700

    thêm ts trang set up pass

--- src/app/set-up-password/set-up-password.component.ts
+++ src/app/set-up-password/set-up-password.component.ts
@@ -8,3 +8,21 @@ import { Component } from '@angular/core';
 export class SetUpPasswordComponent {
 
 }
+const showPassword = document.querySelector<HTMLInputElement>("#show-password");
+const passwordField = document.querySelector<HTMLInputElement>("#password");
+
+showPassword?.addEventListener("click", function(this: HTMLElement){
+    this.classList.toggle("fa-eye-slash");
+    const type = passwordField?.getAttribute("type") === "password" ? "text" : "password";
+    passwordField?.setAttribute("type", type);
+});
+
+
+const showPassword1 = document.querySelector<HTMLInputElement>("#show-password1");
+const passwordField1 = document.querySelector<HTMLInputElement>("#password1");
+
+showPassword1?.addEventListener("click", function(this: HTMLElement){
+    this.classList.toggle("fa-eye-slash");
+    const type = passwordField1?.getAttribute("type") === "password" ? "text" : "password";
+    passwordField1?.setAttribute("type", type);
+});
