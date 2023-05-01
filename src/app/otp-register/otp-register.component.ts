ecb40bd85adbe2a14a9b7ece2ec0eb23444391dd
vanhieu154 <117420642+vanhieu154@users.noreply.github.com>
2023-05-01 11:14:53 +0700

    update

--- src/app/otp-register/otp-register.component.ts
+++ src/app/otp-register/otp-register.component.ts
@@ -37,33 +37,33 @@ export class OtpRegisterComponent {
   }
 
 }
-function OTPInput() {
-  const inputs = document.querySelectorAll<HTMLInputElement>('#otp > *[id]');
+// function OTPInput() {
+//   const inputs = document.querySelectorAll<HTMLInputElement>('#otp > *[id]');
 
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keydown', function (event: KeyboardEvent) {
-        if (event.key === "Backspace") {
-          inputs[i].value = '';
-          if (i !== 0)
-            (inputs[i - 1] as HTMLInputElement).focus();
-        } else {
-          if (i === inputs.length - 1 && inputs[i].value !== '') {
-            return true;
-          } else if (event.keyCode > 47 && event.keyCode < 58) { //0-9 only
-            inputs[i].value = event.key;
-            if (i !== inputs.length - 1)
-              (inputs[i + 1] as HTMLInputElement).focus();
-            event.preventDefault();
-          } else if (event.keyCode > 64 && event.keyCode < 91) {
-            inputs[i].value = String.fromCharCode(event.keyCode);
-            if (i !== inputs.length - 1)
-              (inputs[i + 1] as HTMLInputElement).focus();
-            event.preventDefault();
-          }
-        }
-      });
-  }
-  return;
-}
+//   for (let i = 0; i < inputs.length; i++) {
+//     inputs[i].addEventListener('keydown', function (event: KeyboardEvent) {
+//         if (event.key === "Backspace") {
+//           inputs[i].value = '';
+//           if (i !== 0)
+//             (inputs[i - 1] as HTMLInputElement).focus();
+//         } else {
+//           if (i === inputs.length - 1 && inputs[i].value !== '') {
+//             return true;
+//           } else if (event.keyCode > 47 && event.keyCode < 58) { //0-9 only
+//             inputs[i].value = event.key;
+//             if (i !== inputs.length - 1)
+//               (inputs[i + 1] as HTMLInputElement).focus();
+//             event.preventDefault();
+//           } else if (event.keyCode > 64 && event.keyCode < 91) {
+//             inputs[i].value = String.fromCharCode(event.keyCode);
+//             if (i !== inputs.length - 1)
+//               (inputs[i + 1] as HTMLInputElement).focus();
+//             event.preventDefault();
+//           }
+//         }
+//       });
+//   }
+//   return;
+// }
 
-OTPInput();
+// OTPInput();
